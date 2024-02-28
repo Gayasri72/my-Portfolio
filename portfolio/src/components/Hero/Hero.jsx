e57767432097e199ff30aee2127caaa9eefd4483
Gayasri72 <gayasripethum1@gmail.com>
2024-02-28 11:47:00 +0530

    day 2 progress

---
+++ portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from "./Hero.module.css"
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I'am Gayasri</h1>
+            <p className={styles.description}>I'am a fullstack developer with 5 years of experience in MERN</p>
+            <a className={styles.contactBtn} href="mailto:gayasripethum1@gmail.com">Contact me</a>
+        </div>
+        <img className={styles.heriImg} src={getImageUrl("hero/heroImage.png")} alt="hero image" />
+        <div className={styles.topBlur}></div>
+        <div className={styles.bootomBlur}></div>
+    </section>
+  )
+}
