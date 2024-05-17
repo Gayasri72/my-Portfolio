57548d434da4e9a7a22e93147658e2c8689a7b93
Gayasri72 <gayasripethum1@gmail.com>
2024-05-17 09:05:31 +0530

    updates

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import { getImageUrl } from '../../utils'
-import styles from "./Hero.module.css"
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I'am Gayasri</h1>
-            <p className={styles.description}>I'am a fullstack developer with 5 years of experience in MERN</p>
-            <a className={styles.contactBtn} href="mailto:gayasripethum1@gmail.com">Contact me</a>
-        </div>
-        <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="hero image" />
-        <div className={styles.topBlur}></div>
-        <div className={styles.bottomBlur}></div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'am Gayasri</h1>
+        <p className={styles.description}>
+          Currently pursuing an Honors Degree in
+          Software Engineering at SLIIT University. With a fervent passion for
+          both full-stack development and mobile app development.   
+        </p>
+        <a className={styles.contactBtn} href="mailto:gayasripethum1@gmail.com">
+          Contact me
+        </a>
+      </div>
+      <img
+        className={styles.heroImg}
+        src={getImageUrl("hero/heroImage.png")}
+        alt="hero image"
+      />
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
     </section>
-  )
-}
+  );
+};
